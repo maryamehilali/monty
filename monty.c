38a1b46371051e22894c433be7649e71168316fd
Maryame <elhilali.maryame2@gmail.com>
2023-09-21 10:31:35 -0700

    new line added

--- monty.c
+++ monty.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 	var.monty_file = fopen(argv[1], "r");
 	if (var.monty_file == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	char_read = 0;
